d783b43b1c4575ec61ef4e253efeb04290de745a
katoztmy <kato.eng.dao@gmail.com>
2026-03-22 18:41:11 +0900

    一旦createも作った

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ItemsService } from './items.service';
+import { Item } from './items.model';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+  constructor(private readonly itemsService: ItemsService) {}
+  @Get()
+  findAll(): Item[] {
+    return this.itemsService.findAll();
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: 'ON_SALE',
+    };
+    return this.itemsService.create(item);
+  }
+}
